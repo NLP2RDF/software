b27b14dc24f8c70a5dc519f0c88758fdc25cf4db
kurzum <kurzum@github.com>
2013-07-03 00:27:41 +0200

    added all tests

--- core/jena/src/main/java/org/nlp2rdf/core/SPARQLValidator.java
+++ core/jena/src/main/java/org/nlp2rdf/core/SPARQLValidator.java
@@ -14,6 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -23,7 +25,7 @@ import java.util.Scanner;
 public class SPARQLValidator {
     private static Logger log = LoggerFactory.getLogger(SPARQLValidator.class);
 
-    public static Model validate(OntModel toBeValidated, String query, String logPrefix) {
+    public Model validate(OntModel toBeValidated, String query, String logPrefix) {
         OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM, ModelFactory.createDefaultModel());
         NIFNamespaces.addRLOGPrefix(model);
         model.setNsPrefix("mylog", logPrefix);
@@ -41,14 +43,31 @@ public class SPARQLValidator {
     }
 
     public static void main(String[] args) {
-        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM, ModelFactory.createDefaultModel());
-        String theString = convertStreamToString(SPARQLValidator.class.getClassLoader()
-                .getResourceAsStream("sparql/hasReferenceContext.sparql")
-        );
-        model.read(SPARQLValidator.class.getClassLoader().getResourceAsStream("erroneous/nif-error-1.ttl"), "", "Turtle");
+        OntModel tobevalidated = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM, ModelFactory.createDefaultModel());
+        OntModel output = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM, ModelFactory.createDefaultModel());
+        output.setNsPrefix("rlog", "http://persistence.uni-leipzig.org/nlp2rdf/ontologies/rlog#");
+        List<String> tests = new ArrayList<String>();
+        tests.add(getQuery("sparqltest/misspelling_wrong_vocab/RFC5147StringMisspelling.sparql"));
 
+        tests.add(getQuery("sparqltest/indices/anchorOfShouldMatchisStringSubstr.sparql"));
+        tests.add(getQuery("sparqltest/indices/beginIndexOfContextShouldBeZero.sparql"));
+        tests.add(getQuery("sparqltest/indices/isStringLength.sparql"));
+        tests.add(getQuery("sparqltest/missing_property/contextHasIsString.sparql"));
+        tests.add(getQuery("sparqltest/missing_property/hasReferenceContext.sparql"));
+        tests.add(getQuery("sparqltest/missing_property/RFC5147StringHasBeginIndex.sparql"));
+        tests.add(getQuery("sparqltest/missing_property/RFC5147StringHasEndIndex.sparql"));
+        tests.add(getQuery("sparqltest/missing_type/contextTypedAsRFC5147String.sparql"));
+        tests.add(getQuery("sparqltest/misspelling_wrong_vocab/RFC5147StringMisspelling.sparql"));
+        tests.add(getQuery("sparqltest/ranges/beginEndIndexAreNonNegativeInteger.sparql"));
+        tobevalidated.read(SPARQLValidator.class.getClassLoader().getResourceAsStream("erroneous/nif-error-1.ttl"), "", "Turtle");
+        for (String q : tests) {
+            System.out.println(q);
+            output.add(new SPARQLValidator().validate(tobevalidated, q, "http://example.org/ex#"));
+            break;
+
+        }
         RDFWriter rw = new JenaReadersWriters.RDFWriterRIOT_Turtle();
-        rw.write(validate(model, theString, "http://example.org/ex#"), System.out, "");
+        rw.write(output, System.out, "");
         //QueryExecution qe = QueryExecutionFactory.create(theString, ModelFactory.createDefaultModel());
 
         //System.out.println(qe.execConstruct());
@@ -62,6 +81,11 @@ public class SPARQLValidator {
         //Op op = SSE.readOp("anchorOfShouldMatchisStringSubstr.sparql");
 
     }
+
+    public static String getQuery(String file) {
+        return convertStreamToString(SPARQLValidator.class.getClassLoader().getResourceAsStream(file));
+    }
+
 
     public static String convertStreamToString(InputStream is) {
         Scanner s = new Scanner(is).useDelimiter("\\A");
