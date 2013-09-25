ee810a6a7ef593e1989abe58658cdcbc685f314b
kurzum <kurzum@github.com>
2013-09-25 15:02:37 +0200

    some renaming

--- java-maven/implementation/stanford-core/publish.sh
+++
@@ -1,6 +0,0 @@
-# sync run
-mvn clean assembly:single
-mv target/*.jar nif-2.0-stanford-core-cli-beta.jar
-rsync -rav nif-2.0-stanford-core-cli-beta.jar root@prod1:/var/www/nlp2rdf_data/nlp2rdf_static/releases/nif-2.0-stanford-core-cli-beta.jar
-
- 
