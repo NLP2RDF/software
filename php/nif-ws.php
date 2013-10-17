be94a7f388fbd58c4532e389ea409f63fdf7d1f7
kurzum <kurzum@github.com>
2013-10-18 06:25:07 +1100

    renamed msClassref

--- php/nif-ws.php
+++ php/nif-ws.php
@@ -165,7 +165,7 @@ if (@$_REQUEST['dummy']==="true"){
 		nif:referenceContext <".$prefix."char=0,39>  ;
 		itsrdf:taIdentRef <http://dbpedia.org/resource/Natalie_Portman> ;
 		itsrdf:taClassRef <http://nerd.eurecom.fr/ontology#Person> , dbo:Actor, dbo:Artist, dbo:Person, dbo:Agent, owl:Thing ;
-		nif:taMscRef dbo:Actor .
+		nif:taMsClassRef dbo:Actor .
 		
 	<http://dbpedia.org/resource/Natalie_Portman> a <http://nerd.eurecom.fr/ontology#Person> , dbo:Actor, dbo:Artist, dbo:Person, dbo:Agent, owl:Thing .
 	<http://nerd.eurecom.fr/ontology#Person>  <http://nerd.eurecom.fr/ontology#isCoreClass> \"1\" . ";
