fe0d6986ac87758a2911c066d0e45acf320d6fd9
der-bruemmer <der.bruemmer@gmail.com>
2014-07-23 13:41:36 +0200

    ConLL dependency converter first version, OliaInterface + Stts interface implementation for choosable tagsets

---
+++ java-maven/vocabularymodule/OLiA/src/main/java/org/nlp2rdf/vm/olia/models/OliaInterface.java
@@ -0,0 +1,14 @@
+package org.nlp2rdf.vm.olia.models;
+
+import org.apache.commons.collections.map.MultiValueMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface OliaInterface { 
+	
+	public MultiValueMap getLinks();
+	public MultiValueMap getTags();
+	
+}
+	
