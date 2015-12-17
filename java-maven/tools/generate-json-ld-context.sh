a829ce8d42b397e1cd012878b14ef5892273ef0b
kurzum <kurzum@gmail.com>
2015-12-17 11:47:03 +0100

    corrected script

--- java-maven/tools/generate-json-ld-context.sh
+++ java-maven/tools/generate-json-ld-context.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 
 mvn clean compile
-
-ONTO="OntClasses DatatypeProperties ObjectProperties AnnotationProperties"
-for one in $ONTO
-	do 
-
-		mvn exec:java -e  -Dexec.mainClass="org.nlp2rdf.tools.Generate" -Dexec.args="-t ${one} -f ../implementation/lexo/src/main/java/org/nlp2rdf/core/vocab/LExO${one}.java -o http://persistence.uni-leipzig.org/nlp2rdf/ontologies/vm/lexo#"
-	
-		mvn exec:java -e   -Dexec.mainClass="org.nlp2rdf.tools.Generate" -Dexec.args="-t ${one} -f ../core/jena/src/main/java/org/nlp2rdf/core/vocab/NIF${one}.java -o http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#"
-
-		mvn exec:java -e  -Dexec.mainClass="org.nlp2rdf.tools.Generate" -Dexec.args="-t ${one} -f ../core/jena/src/main/java/org/nlp2rdf/core/vocab/RLOG${one}.java -o http://persistence.uni-leipzig.org/nlp2rdf/ontologies/rlog#"
-
-
-	done
+mvn exec:java -e  -Dexec.mainClass="org.nlp2rdf.tools.Generate" -Dexec.args="-t json-ld-context -f nif-core-context.jsonld -o http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core# -o https://raw.githubusercontent.com/w3c/itsrdf/master/its-rdf.rdf" 
