c7a020a3a95519d1c771df7853efd8777a2dec76
kurzum <kurzum@gmail.com>
2015-12-31 00:05:35 +0100

    working bundle

---
+++ java-maven/implementation/bundle/src/main/java/org/nlp2rdf/implementation/snowball/Dummy.java
@@ -0,0 +1,30 @@
+/******************************************************************************/
+/*  Copyright (C) 2010-2011, Sebastian Hellmann                               */
+/*                                                                            */
+/*  Licensed under the Apache License, Version 2.0 (the "License");           */
+/*  you may not use this file except in compliance with the License.          */
+/*  You may obtain a copy of the License at                                   */
+/*                                                                            */
+/*      http://www.apache.org/licenses/LICENSE-2.0                            */
+/*                                                                            */
+/*  Unless required by applicable law or agreed to in writing, software       */
+/*  distributed under the License is distributed on an "AS IS" BASIS,         */
+/*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  */
+/*  See the License for the specific language governing permissions and       */
+/*  limitations under the License.                                            */
+/******************************************************************************/
+
+package org.nlp2rdf.implementation.bundle;
+
+import java.io.IOException;
+
+/**
+ * Why is this class even here? 
+ * Well if there aren't any java files to compile, maven doesn't create the /software/java-maven/implementation/bundle/target/classes folder, which is a problem for jetty
+ */
+public class Dummy {
+    
+public static void main(String[] args) throws IOException {
+        
+    }
+}
